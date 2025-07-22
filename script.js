302d8dbca691f7c817944b1c9bcb88f6025994e6
orpheusdark <117748842+orpheusdark@users.noreply.github.com>
2025-07-22 18:26:35 +0530

    Fix Image Visibility and Add Skill Icons

--- script.js
+++ script.js
@@ -659,7 +659,9 @@ function loadProjects() {
                             </a>
                             <a href="${project.demo}" target="_blank" class="project-link">
                                 <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-                                    <path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"/>
+                                <div class="skill-progress-container">
+                                    <div class="skill-progress" data-level="${skill.level}"></div>
+                                </div>
                                 </svg>
                             </a>
                         </div>
